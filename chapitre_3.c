77e0293c0c93eb3e55a4f32a4a7cabd489ed7a0c
Valentin Kaelin <kalanehd@gmail.com>
2021-05-06 11:31:48 +0200

    refactor: corrections of ex 3.4

--- chapitre_3.c
+++ chapitre_3.c
@@ -74,18 +74,22 @@ int ex3_3(void) {
 	return EXIT_SUCCESS;
 }
 
-typedef struct date {
+#define FORMAT_DATE "%02" PRIu8 ".%02" PRIu8 ".%04" PRIu16
+
+typedef struct {
 	uint8_t jour;
 	uint8_t mois;
 	uint16_t annee;
 } Date;
 
 bool estBissextile(uint16_t annee) {
-	return annee % 400 == 0 || (annee % 4 == 0 && annee % 100 != 0);
+	return annee % 400 == 0 || (annee % 4 == 0 && annee % 100);
 }
 
-uint8_t nbJoursParMois(uint8_t noMois, uint16_t annee) {
-	switch (noMois) {
+uint8_t nbJoursDansMois(uint8_t mois, uint16_t annee) {
+	// Version du prof: tableau static contenant le nbr de jour par mois + check
+	// bissextile
+	switch (mois) {
 		case 2:
 			return estBissextile(annee) ? 29 : 28;
 		case 4:
@@ -98,35 +102,40 @@ uint8_t nbJoursParMois(uint8_t noMois, uint16_t annee) {
 	}
 }
 
-void afficherDate(const Date* date) {
-	printf("%02d.%02d.%04d", date->jour, date->mois, date->annee);
+const char* toString(const Date* d) {
+	char* str = (char*) calloc(11, sizeof(char));
+	if (str) {
+		sprintf(str, FORMAT_DATE, d->jour, d->mois, d->annee);
+	}
+	return str;
 }
 
-Date lendemain(Date date) {
-//	Date* nouvelleDate = (Date*) malloc(sizeof(Date));
-	if (++date.jour > nbJoursParMois(date.mois, date.annee)) {
-		date.jour = 1;
-		++date.mois;
+Date lendemain(const Date* d) {
+	Date lendemain;
+	if (d->jour != nbJoursDansMois(d->mois, d->annee)) {
+		lendemain = (Date) {(uint8_t) (d->jour + 1), d->mois, d->annee};
+	} else if (d->mois < 12) {
+		lendemain = (Date) {1, (uint8_t) (d->mois + 1), d->annee};
+	} else {
+		lendemain = (Date) {1, 1, (uint16_t) (d->annee + 1)};
 	}
-	if (date.mois > 12) {
-		date.mois = 1;
-		++date.annee;
-	}
-	return date;
+	return lendemain;
 }
 
-void afficher_lendemain(const Date* date) {
-	printf("Le lendemain du ");
-	afficherDate(date);
-	Date prochainJour = lendemain(*date);
-	printf(" est le ");
-	afficherDate(&prochainJour);
-	printf("\n");
+void test_ex3_4(const Date* d) {
+	Date leLendemain = lendemain(d);
+	const char* str1 = toString(d);
+	const char* str2 = toString(&leLendemain);
+	printf("Le lendemain du %s est le %s\n", str1, str2);
+	free((char*) str1);
+	free((char*) str2);
 }
 
 int ex3_4(void) {
-	Date date = {30, 4, 2016};
-	afficher_lendemain(&date);
+	test_ex3_4(&(Date) {28, 2, 2000});
+	test_ex3_4(&(Date) {29, 2, 2000});
+	test_ex3_4(&(Date) {30, 11, 2000});
+	test_ex3_4(&(Date) {31, 12, 2000});
 
 	return EXIT_SUCCESS;
 }
